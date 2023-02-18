openseedbox/openseedbox-server

commit 9b3b5515b7050826d767b2a0af5067e53c5497c2

TransmissionBackend: fix isRunning (openseedbox/openseedbox#105)
Just grepping for a number in the process list in a non-containerized
environment isn't the best way to find out if the backend is running. ;)

$ ps -A | grep 1 | wc -l
361

While checking the backend's availibility through RPC is a good point,
- and the initial commit (d8825f07009b) has it as a hint - it's not
recommended for Transmission, which is retardedly slow on slow hard
disks (due to IO thread and RPC thread being the same)!
See todo.txt at openseedbox/openseedbox@4b12abd0d75 and
transmission/transmission#2462 also!

Fixes: 98fb99538fcd ("mad progress")

app/com/openseedbox/backend/transmission/TransmissionBackend.java
@@ -123,7 +123,7 @@ public class TransmissionBackend implements ITorrentBackend {
 		if (pidFile.exists()) {
 			try {
 				String pid = getDaemonPID(pidFile);
-				return !StringUtils.isEmpty(Util.executeCommand("ps -A | grep " + pid).trim());
+				return binaryName.equals(Util.executeCommand("head -n1 -z /proc/" + pid + "/cmdline").trim());
 			} catch (IOException ex) {
 				Logger.error("Unable to read daemon.pid file", ex);
 				return false;

commit 4c93a9a4fc2b234a4444495f2d3f4a08a4f2fe63

TransmissionBackend: fix isRunning within Docker multiarch build too
With multiarch build the cmdline starts with "qemu", e.g.:
/usr/bin/qemu-arm-static /usr/bin/transmission-daemon --config-dir /media/openseedbox --pid-file /media/openseedbox/daemon.pid

See openseedbox/openseedbox#114

Fixes: 9b3b5515b705 ("TransmissionBackend: fix isRunning
(openseedbox/openseedbox#105)")

app/com/openseedbox/backend/transmission/TransmissionBackend.java
@@ -123,7 +123,11 @@ public class TransmissionBackend implements ITorrentBackend {
 		if (pidFile.exists()) {
 			try {
 				String pid = getDaemonPID(pidFile);
-				return binaryName.equals(Util.executeCommand("head -n1 -z /proc/" + pid + "/cmdline").trim());
+				String headCmdLine = Util.executeCommand("head -n1 -z /proc/" + pid + "/cmdline").trim();
+				return binaryName.equals(headCmdLine)
+					|| headCmdLine.contains("qemu")
+					&& Util.executeCommand("cat /proc/" + pid + "/cmdline").trim().contains(binaryName)
+				;
 			} catch (IOException ex) {
 				Logger.error("Unable to read daemon.pid file", ex);
 				return false;